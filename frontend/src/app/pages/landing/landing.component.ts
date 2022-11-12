9518d8cd018a77ec8dfb35e2feec81bec72fa5c9
juan <juan.molina159@gmail.com>
2022-11-12 16:30:51 -0300

    Update landing.component.ts

--- frontend/src/app/pages/landing/landing.component.ts
+++ frontend/src/app/pages/landing/landing.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-<<<<<<< HEAD
-
   constructor() { }
 
-=======
   saldo = 1000;
   constructor() { }
->>>>>>> develop
   ngOnInit(): void {
   }
 
